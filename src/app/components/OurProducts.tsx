24bb8d83b69f4d3c59f7f7bda2dcd1e5923a76e7
habtesellasie <havecodesoft@gmail.com>
2024-08-07 21:36:21 +0300

    update the our products section

--- src/app/components/OurProducts.tsx
+++ src/app/components/OurProducts.tsx
@@ -32,18 +32,18 @@ const OurProducts = () => {
                   alt={`product image ${productIndex}`}
                   className='size-[25rem] object-cover rounded-md transition border-[1px] border-gray-300 max-sm:w-full'
                 />
-                <div className='w-full flex flex-col items-center justify-center'>
-                  {product?.name && (
-                    <span className='bottom-0 text-white p-2 text-sm text-center left-1/2  bg-black/60 backdrop-blur-sm'>
-                      {product?.name}
-                    </span>
-                  )}
-                  {product?.name && (
-                    <p className='w-full max-w-lg py-2 text-sm text-center shadow inline-block p-12'>
-                      {product?.description}
-                    </p>
-                  )}
-                </div>
+              </div>
+              <div className='w-full flex flex-col items-center justify-center'>
+                {product?.name && (
+                  <span className='bottom-0 p-2 text-sm text-center left-1/2 text-black font-bold'>
+                    {product?.name}
+                  </span>
+                )}
+                {product?.name && (
+                  <p className='w-full max-w-lg py-2 text-sm text-center inline-block p-12'>
+                    {product?.description}
+                  </p>
+                )}
               </div>
             </div>
           );
